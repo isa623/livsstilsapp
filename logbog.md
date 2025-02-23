78d1acb3f506b853bd74443670e4d3e8e69ae25a
isa623 <NEXT16325@edu.nextkbh.dk>
2025-02-23 17:45:44 +0100

    Oprette log bog og template filer

---
+++ logbog.md
@@ -0,0 +1,2 @@
+# isabellas logbog 
+* 23-02-2025: idag har jeg lavet prjektbeskrivelsen til eksamen. 
---
+++ sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	circle(50, 50, 50)
+}
